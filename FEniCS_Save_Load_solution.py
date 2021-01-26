3e7f1f2b016279e23fe514d354a90a37761e6668
XuanQuangPham91 <xuanquangpham91@gmail.com>
2021-01-26 21:43:54 +0900

    update save load 3rd

--- FEniCS_Save_Load_solution.py
+++ FEniCS_Save_Load_solution.py
@@ -1,10 +1,11 @@
 from dolfin import *
+import matplotlib.pyplot as plt
 
 
 def Save_XDMF(V, title):
     U = Function(V)
     # input_file = XDMFFile(mesh.mpi_comm(), "solution/RB/%s.xdmf" % title)
-    input_file = XDMFFile("solution/RB/%s.xdmf" % title)
+    input_file = XDMFFile("solution/%s.xdmf" % title)
     input_file.write(U, "solution")
     input_file.close()
 
@@ -12,26 +13,50 @@ def Save_XDMF(V, title):
 def Load_XDMF(V, title):
     U = Function(V)
     # input_file = XDMFFile(mesh.mpi_comm(), "solution/RB/%s.xdmf" % title)
-    input_file = XDMFFile("solution/RB/%s.xdmf" % title)
+    input_file = XDMFFile("solution/%s.xdmf" % title)
     input_file.read(U, "solution")
     input_file.close()
     return U
 
 
-def Save_HDF5(V, mesh, title=None):
-    # Load solution
-    U = Function(V)
-    input_file = HDF5File(mesh.mpi_comm(), "solution/RB/%s.h5" % title, "r")
+def Save_HDF5(u, mesh, title=None):
+    output_file = HDF5File(mesh.mpi_comm(), "solution/%s.h5" % title, "w")
     # input_file = HDF5File("solution/%s.h5" % title, "r")
-    input_file.read(U, "solution")
-    input_file.close()
+    output_file.write(u, "solution")
+    output_file.close()
 
 
 def Load_HDF5(V, mesh, title=None):
-    # Load solution
     U = Function(V)
-    input_file = HDF5File(mesh.mpi_comm(), "solution/RB/%s.h5" % title, "r")
+    input_file = HDF5File(mesh.mpi_comm(), "solution/%s.h5" % title, "r")
     # input_file = HDF5File("solution/%s.h5" % title, "r")
     input_file.read(U, "solution")
     input_file.close()
     return U
+
+
+'''FEniCS plot '''
+
+
+def my_FEniCS_plot(number_of_figure, u, title):
+    plt.figure(number_of_figure)
+    plt.colorbar(plot(u, title='%s' % title))
+    # plt.axis('tight')
+    # plt.legend()
+    # plt.grid(True)
+    plt.title(title)
+    plt.xlabel('$y_1$')
+    plt.ylabel('$y_2$')
+    plt.savefig('solution/%s.png' % title, format='png')
+
+
+def my_FEniCS_plot_mode(number_of_figure, u, title, mode):
+    plt.figure(number_of_figure)
+    plt.colorbar(plot(u, title='%s' % title, mode=mode))
+    # plt.axis('tight')
+    # plt.legend()
+    # plt.grid(True)
+    plt.title(title)
+    plt.xlabel('$y_1$')
+    plt.ylabel('$y_2$')
+    plt.savefig('solution/%s.png' % title, format='png')
---
+++ __init__.py
@@ -0,0 +1,4 @@
+from . import FEniCS_plot
+from .FEniCS_Save_Load_solution import (Save_XDMF, Save_HDF5, Load_HDF5,
+                                        Load_XDMF, my_FEniCS_plot,
+                                        my_FEniCS_plot_mode)
