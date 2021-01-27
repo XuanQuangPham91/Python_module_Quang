3d1831b91ecc343bf2c15be26db8bb22eaf943b9
XuanQuangPham91 <xuanquangpham91@gmail.com>
2021-01-27 10:59:29 +0900

    update nonmatching_interpolation

--- FEniCS_plot.py
+++ FEniCS_plot.py
@@ -5,53 +5,76 @@ import os
 
 print(os.getcwd())
 
+# def my_plot(
+#     number_of_figure,
+#     u,
+#     title,
+#     reduced_problem='False',
+#     savefig="True",
+# ):
+#     if reduced_problem == 'reduced_problem':
+#         # u = reduced_problem
+#         p = plot(u, reduced_problem=reduced_problem, title=title)
+#     elif reduced_problem == 'False':
+#         p = plot(u, title='%s' % title)
+#     plt.figure(number_of_figure)
+#     plt.colorbar(p)
+#     # plt.axis('tight')
+#     # plt.legend()
+#     plt.grid(True)
+#     plt.title(title)
+#     plt.xlabel('$x$')
+#     plt.ylabel('$y$')
+#     if savefig == 'True':
+#         plt.savefig('20210118_RB_results/%s.png' % title, format='png')
+#     else:
+#         pass
 
-def my_plot(
-    number_of_figure,
-    u,
-    title,
-    reduced_problem='False',
-    savefig="True",
-):
-    if reduced_problem == 'reduced_problem':
-        # u = reduced_problem
-        p = plot(u, reduced_problem=reduced_problem, title=title)
-    elif reduced_problem == 'False':
-        p = plot(u, title='%s' % title)
+# def my_plot_mode(number_of_figure,
+#                  u,
+#                  title,
+#                  mode,
+#                  reduced_problem='False',
+#                  savefig="True"):
+#     if reduced_problem == 'reduced_problem':
+#         # u = reduced_problem
+#         p = plot(u, reduced_problem=reduced_problem, title=title, mode=mode)
+#     elif reduced_problem == 'False':
+#         p = plot(u, title='%s' % title, mode=mode)
+#     plt.figure(number_of_figure)
+#     # plt.axis('tight')
+#     # plt.legend()
+#     plt.grid(True)
+#     plt.title(title)
+#     plt.xlabel('$x$')
+#     plt.ylabel('$y$')
+#     plt.colorbar(p)
+#     if savefig == 'True':
+#         plt.savefig('20210118_RB_results/%s.png' % title, format='png')
+#     else:
+#         pass
+'''FEniCS plot '''
+
+
+def my_FEniCS_plot(number_of_figure, u, title):
     plt.figure(number_of_figure)
-    plt.colorbar(p)
+    plt.colorbar(plot(u, title='%s' % title))
     # plt.axis('tight')
     # plt.legend()
-    plt.grid(True)
+    # plt.grid(True)
     plt.title(title)
-    plt.xlabel('$x$')
-    plt.ylabel('$y$')
-    if savefig == 'True':
-        plt.savefig('20210118_RB_results/%s.png' % title, format='png')
-    else:
-        pass
+    plt.xlabel('$y_1$')
+    plt.ylabel('$y_2$')
+    plt.savefig('solution/%s.png' % title, format='png')
 
 
-def my_plot_mode(number_of_figure,
-                 u,
-                 title,
-                 mode,
-                 reduced_problem='False',
-                 savefig="True"):
-    if reduced_problem == 'reduced_problem':
-        # u = reduced_problem
-        p = plot(u, reduced_problem=reduced_problem, title=title, mode=mode)
-    elif reduced_problem == 'False':
-        p = plot(u, title='%s' % title, mode=mode)
+def my_FEniCS_plot_mode(number_of_figure, u, title, mode):
     plt.figure(number_of_figure)
+    plt.colorbar(plot(u, title='%s' % title, mode=mode))
     # plt.axis('tight')
     # plt.legend()
-    plt.grid(True)
+    # plt.grid(True)
     plt.title(title)
-    plt.xlabel('$x$')
-    plt.ylabel('$y$')
-    plt.colorbar(p)
-    if savefig == 'True':
-        plt.savefig('20210118_RB_results/%s.png' % title, format='png')
-    else:
-        pass
+    plt.xlabel('$y_1$')
+    plt.ylabel('$y_2$')
+    plt.savefig('solution/%s.png' % title, format='png')
--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from . import FEniCS_plot
+from .FEniCS_plot import (my_plot, my_plot_mode, my_FEniCS_plot,
+                          my_FEniCS_plot_mode)
 from .FEniCS_Save_Load_solution import (Save_XDMF, Save_HDF5, Load_HDF5,
-                                        Load_XDMF, my_FEniCS_plot,
-                                        my_FEniCS_plot_mode)
+                                        Load_XDMF)
+from .nonmatching_interpolation import (test_functional2D, test_functional3D)
