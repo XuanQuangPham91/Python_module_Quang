48fe09a3a5d572d0bc635480c712483e825a36ad
XuanQuangPham91 <xuanquangpham91@gmail.com>
2021-01-27 11:04:23 +0900

    change name file

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .FEniCS_plot import (my_plot, my_plot_mode, my_FEniCS_plot,
                           my_FEniCS_plot_mode)
-from .FEniCS_Save_Load_solution import (Save_XDMF, Save_HDF5, Load_HDF5,
+from .FEniCS_save_load_solution import (Save_XDMF, Save_HDF5, Load_HDF5,
                                         Load_XDMF)
-from .nonmatching_interpolation import (test_functional2D, test_functional3D)
+from .FEniCS_nonmatching_interpolation import (test_functional2D,
+                                               test_functional3D)
