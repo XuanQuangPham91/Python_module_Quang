a241390220a5d9322c17a05aedf0b5e4a37743e7
XuanQuangPham91 <xuanquangpham91@gmail.com>
2021-01-27 20:25:16 +0900

    update containing folder

--- __init__.py
+++ __init__.py
@@ -1,28 +1,28 @@
 try:
-    from .FEniCS_plot import FEniCS_plot_mode
+    from .FEniCS_module.FEniCS_plot import FEniCS_plot_mode
 except:
     print('Failed to import FEniCS_plot')
 
 try:
-    from .FEniCS_save_load_solution import \
+    from .FEniCS_module.FEniCS_save_load_solution import \
         Save_XDMF, Save_HDF5, \
         Load_HDF5, Load_XDMF
 except:
     print('Failed to import FEniCS_save_load_solution')
 
 # try:
-#     from .FEniCS_nonmatching_interpolation import \
+#     from .FEniCS_module.FEniCS_nonmatching_interpolation import \
 #         test_functional2D, test_functional3D
 # except:
 #     print('Failed to import FEniCS_nonmatching_interpolation')
 
 # try:
-#     from .FEniCS_lagrange_interpolator import \
+#     from .FEniCS_module.FEniCS_lagrange_interpolator import \
 #         test_functional2D, test_functional3D
 # except:
 #     print('Failed to import FEniCS_lagrange_interpolator')
 
 try:
-    from .FEniCS_read_XDMFFile import Read_RB_XDMFFile
+    from .FEniCS_module.FEniCS_read_XDMFFile import Read_RB_XDMFFile
 except:
     print('Failed to import Read_XDMFFile')
